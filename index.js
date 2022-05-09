758b8333dbdcd54bebf61541d9ece86fb0656862
Sakib Md Rezowan <sakibmdrezowan@gmail.com>
2022-05-09 13:54:20 +0600

    delete

--- index.js
+++ index.js
@@ -16,43 +16,62 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 async function run() {
-    try {
-      await client.connect();
-      const productCollection = client.db("products").collection("product");
-  
-      app.get("/product", async (req, res) => {
-        const query = {};
-        const cursor = productCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
-      });
-  
-      app.get("/product/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const product = await productCollection.findOne(query);
-        res.send(product);
-      });
-  
-      // POST
-      app.post("/product", async (req, res) => {
-        const newProduct = req.body;
-        const result = await productCollection.insertOne(newProduct);
-        res.send(result);
-      });
-  
+  try {
+    await client.connect();
+    const productCollection = client.db("products").collection("product");
+    //get all data
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    //get single data
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    // create a new data
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    // update product
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          suplier: updatedProduct.suplierName,
+          qantity: updatedProduct.quantity,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
     //   DELETE
-    //   app.delete("/product/:id", async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = { _id: ObjectId(id) };
-    //     const result = await productCollection.deleteOne(query);
-    //     res.send(result);
-    //   });
-    } finally {
-    }
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
   }
-  
-  run().catch(console.dir);
+}
+
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Running werehouse");
 });
